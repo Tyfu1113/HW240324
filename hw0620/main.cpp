ff2995de1ac9f757c9a85123dff6de861207a056
Tyfu1113 <dottly113@gmail.com>
2024-06-16 17:33:12 +0800

    Initial Commit

---
+++ hw0620/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw0620
+//
+//  Created by 傅梓崵 on 2024/6/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
